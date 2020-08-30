7bfc631ac03af3db50a44c77db589fa76e35e6ff
wooseong <ldntjd1081@naver.com>
2020-08-30 18:29:39 +0900

    express 모듈 설치 및 테스트 서버 구축

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-console.log(
-    "hello world!"
-)
+var http = require("http");
+
+http.createServer(function(request, response) {
+    /*
+    HTTP 헤더 전송
+    HTTP status : 200 : OK
+    Content Type : text/plain
+    */
+   response.writeHead(200, {'Content-Type' : 'text/plain'});
+
+   /*
+    Response Body를 "Hello world"로 설정
+   */
+    response.end("Hello World!");
+}).listen(8081);
+
+console.log("Server running at http://127.0.0.1:8081");
