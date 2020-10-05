95e6bb687fbdb82328f4f20fd18d3fc420654c03
김태중 <xownd6235@naver.com>
2020-10-05 20:29:36 +0900

    페이지 나누기

---
+++ gp/src/pages/Buyer.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import styled from "styled-components"
+
+const Buyer=()=>{
+    return(
+        <div>
+            <h2>Buyer Page</h2>
+        </div>
+    )
+}
+
+export default Buyer;
---
+++ gp/src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Home } from './Home';
+export { default as Buyer } from './Buyer';
+export { default as Seller } from './Seller';
