b7afded68aa13a8864d3cf4e86edf6bc82d1d0d8
김태중 <xownd6235@naver.com>
2020-11-16 00:21:33 +0900

    함수형 => 클래스형 컴포넌트 + 이미지 byte array에서 변환

--- gp/src/components/ShowSomeMusics.js
+++ gp/src/components/ShowSomeMusics.js
@@ -1,25 +1,18 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import styled from "styled-components"
 import { BrowserRouter as Router, Route, Switch, Link, useHistory } from "react-router-dom";
 import Display from "./Display"
 import { element } from 'prop-types';
-<<<<<<< HEAD
-const mm = require('music-metadata-browser');
-const Web3 = require('web3');
-
-
-
-let web3 = new Web3(Web3.givenProvider || "https://localhost:7545");
-var ContractAddr = "0x14E558c491BbdfEF78a743B68ffA8B2722637889";
-var ContractAbi =[
-=======
 import cat from 'ipfs-http-client/src/cat';
+import { withStyles } from '@material-ui/core/styles'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import CircularProgress from '@material-ui/core/CircularProgress'
 const Web3 = require('web3');
 const mm = require('music-metadata-browser');
-let web3 = new Web3(Web3.givenProvider || "https://localhost:8545");
-let ContractAddr = "0x1543843f093715a69a826B3B75b37A12c755b51b";
+let web3 = new Web3(Web3.givenProvider || "https://localhost:7545");
+let ContractAddr = "0x0Aea2CcEa4394A2E4216545d74B8C2DA60c0B850";
 let ContractAbi = [
->>>>>>> 73c6fa495f2f756c592ee7d31dd5d5e20b3f035d
 	{
 		"constant": false,
 		"inputs": [
@@ -183,255 +176,289 @@ let ContractAbi = [
 var Contract = new web3.eth.Contract(ContractAbi, ContractAddr);
 // let tempArr=[];
 const metaDataParse = async (path) => {
-    const audioTrackUrl = "https://ipfs.infura.io/ipfs/" + path;
-    const metadata = await mm.fetchFromUrl(audioTrackUrl);
-    
-    return metadata;
+	const audioTrackUrl = "https://ipfs.infura.io/ipfs/" + path;
+	const metadata = await mm.fetchFromUrl(audioTrackUrl);
+
+	return metadata;
 };
 
-const ShowSomeMusics = () => {
-	let history = useHistory();
+const styles = theme => ({
+	progress: {
+	  margin: theme.spacing(3)
+	}
+  })
 
-	const LinkHandle = (idx, data) => {
-		history.push({
-			pathname: "/buyer/" + idx,
-			data: data
+class ShowSomeMusics extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			ipfsPull: '',
+			completed: 0
+		}
+		// this.props.history=useHistory();
+	}
+	
+	// LinkHandle = (idx, data) => {
+	// 	this.props.history.push({
+	// 		pathname: "/buyer/" + idx,
+	// 		data: data
+	// 	});
+	// }
+
+	stateRefresh = () => {
+		this.setState({
+			ipfsPull: '',
+			completed: 0
 		});
+		this.callApi()
+			.then(res => this.setState({ ipfsPull: res }))
+			.catch(err => console.log(err));
 	}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-	var sampleData = [];
+	componentDidMount() {
+		this.timer = setInterval(this.progress, 20);
+		this.callApi()
+			.then(res => this.setState({ ipfsPull: res }))
+			.catch(err => console.log(err));
+	}
 
-	const metaDataParse = async (path) => {
-		const audioTrackUrl = "https://ipfs.infura.io/ipfs/" + path;
-		const metadata = await mm.fetchFromUrl(audioTrackUrl);
-=======
-	const [ipfsPull,SetipfsPull]=useState(null);
-=======
-	const [ipfsPull,setipfsPull]=useState(null);
->>>>>>> 6112f3f24a67c079499b2b06ea766063b672e8c1
-    
-    
-        
-    //     Contract.methods.showAllSellerList().call({ from: "0xB901e378b66144a8a6583F16f279D0D8f42c509B" })
-    //     .then((result)=> {
-    //         if (result) {
-                    
-    //                 Promise.all(result.path).then((values)=>{
-                    
-    //                 values.map((data,idx)=>{
-    //                     metaDataParse(data).then((metadata)=>{
-    //                         const data={
-    //                             id:idx,
-    //                             imageLocation:"https://placeimg.com/640/480/any",
-    //                             singer:metadata.common.artist,
-    //                             song:metadata.common.title,
-    //                             accountId:result.sellerAddress[idx]
-    //                         }
-    //                         tempArr.push(data);
-    //                     });
-    //                 })
-                    
-    //             });
-    //         } else console.log("error");
->>>>>>> 73c6fa495f2f756c592ee7d31dd5d5e20b3f035d
+	callApi = async () => {
+		let web3 = new Web3(Web3.givenProvider || "https://localhost:7545");
+		var ContractAddr = "0x0Aea2CcEa4394A2E4216545d74B8C2DA60c0B850";
+		var ContractAbi = [
+			{
+				"constant": false,
+				"inputs": [
+					{
+						"name": "key",
+						"type": "address"
+					},
+					{
+						"name": "path",
+						"type": "string"
+					}
+				],
+				"name": "buyerSet",
+				"outputs": [],
+				"payable": false,
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"constant": false,
+				"inputs": [
+					{
+						"name": "key",
+						"type": "address"
+					},
+					{
+						"name": "path",
+						"type": "string"
+					}
+				],
+				"name": "sellerSet",
+				"outputs": [],
+				"payable": false,
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"constant": false,
+				"inputs": [
+					{
+						"name": "recipient",
+						"type": "address"
+					},
+					{
+						"name": "path",
+						"type": "string"
+					}
+				],
+				"name": "songTrade",
+				"outputs": [],
+				"payable": true,
+				"stateMutability": "payable",
+				"type": "function"
+			},
+			{
+				"constant": true,
+				"inputs": [
+					{
+						"name": "path",
+						"type": "string"
+					}
+				],
+				"name": "returnSeller",
+				"outputs": [
+					{
+						"name": "",
+						"type": "address"
+					}
+				],
+				"payable": false,
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"constant": true,
+				"inputs": [],
+				"name": "showAllBuyerList",
+				"outputs": [
+					{
+						"components": [
+							{
+								"name": "buyerAddress",
+								"type": "address[]"
+							},
+							{
+								"name": "path",
+								"type": "string[]"
+							}
+						],
+						"name": "",
+						"type": "tuple"
+					}
+				],
+				"payable": false,
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"constant": true,
+				"inputs": [],
+				"name": "showAllSellerList",
+				"outputs": [
+					{
+						"components": [
+							{
+								"name": "sellerAddress",
+								"type": "address[]"
+							},
+							{
+								"name": "path",
+								"type": "string[]"
+							}
+						],
+						"name": "",
+						"type": "tuple"
+					}
+				],
+				"payable": false,
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"constant": true,
+				"inputs": [
+					{
+						"name": "key",
+						"type": "address"
+					}
+				],
+				"name": "showBuyerList",
+				"outputs": [
+					{
+						"name": "",
+						"type": "string[]"
+					}
+				],
+				"payable": false,
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"constant": true,
+				"inputs": [
+					{
+						"name": "key",
+						"type": "address"
+					}
+				],
+				"name": "showSellerList",
+				"outputs": [
+					{
+						"name": "",
+						"type": "string[]"
+					}
+				],
+				"payable": false,
+				"stateMutability": "view",
+				"type": "function"
+			}
+		];
+		var Contract = new web3.eth.Contract(ContractAbi, ContractAddr);
 
-    // }).then(()=>{
-    //     console.log(tempArr);
-    //     return tempArr;
-    // })};
-    //     return path;
-    // }
-    
-    // const test=async()=>{
-    //     const data=await getIpfsInformations();
-        
-    // }
+		console.log(Contract)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-	Contract.methods.showAllSellerList().call({ from: "0xb1c504a0BEAa50596153da5688607358E1Bb7BB5" })
-		.then(function (result) {
-			if (result) {
-				console.log(result)
-				var pathArrSize = result.path.length;
-				for (let i = 0; i < pathArrSize; i++) {
-					if (result.path[i]) {
-						metaDataParse(result.path[i]).then((metadata) => {
-							// img를 src말고 metadata에서 버퍼형식으로(?) 사용할 방법 강구해야할듯. 그렇지 않으면 ipfs에 앨범을 따로 저장해야함
-=======
-   const rendering = async () => {
-      var ret = await Contract.methods.showAllSellerList().call({ from: "0x4A5F0e92270ee28Bcce3678BE119A087051694Fe" })
-         .then(function (result) {
-            if (result) {
-               Promise.all(result.path).then((values) => {
-                  values.map((data, idx) => {
-                     metaDataParse(data).then((metadata) => {
-                        const data = {
-                           id: idx,
-                           imageLocation: "https://placeimg.com/640/480/any",
-                           singer: metadata.common.artist,
-                           song: metadata.common.title,
-                           accountId: result.sellerAddress[idx]
-                        }
-                        tempArr.push(data);
-                     });
-                  });
-               })
-            } else console.log("error");
-         }).then(() => {
-            // console.log(tempArr)
-            return tempArr;
-         });
-      return ret;
-   }
->>>>>>> 73c6fa495f2f756c592ee7d31dd5d5e20b3f035d
-=======
-//    const rendering = async () => {
-// 	  var ret = await Contract.methods.showAllSellerList().call({ from: "0x4A5F0e92270ee28Bcce3678BE119A087051694Fe" })
-//          .then(function (result) {
-//             if (result) {
-//                Promise.all(result.path).then((values) => {
-//                   values.map((data, idx) => {
-//                      metaDataParse(data).then((metadata) => {
-//                         const data = {
-//                            id: idx,
-//                            imageLocation: "https://placeimg.com/640/480/any",
-//                            singer: metadata.common.artist,
-//                            song: metadata.common.title,
-//                            accountId: result.sellerAddress[idx]
-//                         }
-//                         tempArr.push(data);
-//                      });
-//                   });
-//                })
-//             } else console.log("error");
-//          }).then(() => {
-//             // console.log(tempArr)
-//             return tempArr;
-//          });
-//       return ret;
-//    }
->>>>>>> 6112f3f24a67c079499b2b06ea766063b672e8c1
-
-//    useEffect(() => {
-//       rendering().then((res)=>{
-//         //  let newIpfsPull=[...ipfsPull,res];
-//          SetipfsPull(res);
-//       });
-//    }, []);
-	
-	useEffect(() => {
-		Contract.methods.showAllSellerList().call({ from: "0xB901e378b66144a8a6583F16f279D0D8f42c509B" })
-		.then((result)=>{
-			let tempArr=[];
-			Promise.all(result.path).then(ipfsHashArray=>{
-				ipfsHashArray.map((ipfsHash,idx)=>{
-					metaDataParse(ipfsHash).then(metaData=>{
-						const data = {
-							id: idx,
-							imageLocation: "https://placeimg.com/640/480/any",
-							singer: metaData.common.artist,
-							song: metaData.common.title,
-							accountId: result.sellerAddress[idx]
-						}
-						// let newArr=[...tempArr,data];
-						// setipfsPull(newArr);
-						// tempArr=newArr;
-						tempArr.push(data);
+		let ret = await Contract.methods.showAllSellerList().call({ from: "0x6c329924357A118Cb25F69512fBB4a347A3DA91B" })
+			.then(async (result) => {
+				let tempArr = [];
+				let ret = await Promise.all(result.path).then(ipfsHashArray => {
+					ipfsHashArray.map((ipfsHash, idx) => {
+						metaDataParse(ipfsHash).then(metaData => {
+							var imgsrc = "data:image/png;base64," + btoa([].reduce.call(new Uint8Array(metaData.common.picture[0].data),function(p,c){return p+String.fromCharCode(c)},''))
+							const data = {
+								id: idx,
+								imageLocation: imgsrc,
+								// imageLocation: "https://placeimg.com/640/480/any",
+								singer: metaData.common.artist,
+								song: metaData.common.title,
+								accountId: result.sellerAddress[idx]
+							}
+							// console.log(metaData.common.picture[0].data);
+							tempArr.push(data);
+						})
 					})
+					return tempArr;
 				})
-				return tempArr;
-			}).then(metaData=>{
-				console.log(metaData);
-				setipfsPull(metaData)
-				return ipfsPull
-			}).then(()=>console.log(ipfsPull))
-		})
-	}, []);
+				return ret;
+			})
 
-	return (
-<<<<<<< HEAD
-		<Wrapper>
-			<TitleOfComponent>
-				Start Listening
-            </TitleOfComponent>
-			<MusicInformations>
-				{
-					sampleData.map((data, idx) => {
-						if (data) {
+		console.log(ret)
+		return ret;
+	}
+
+
+	progress = () => {
+		const { completed } = this.state;
+		this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+	}
+
+	render() {
+		const { classes } = this.props;
+		return (
+			<Wrapper>
+				<TitleOfComponent>
+					Start Listening
+				</TitleOfComponent>
+				<MusicInformations>
+					{this.state.ipfsPull ?
+						this.state.ipfsPull.map((data, idx) => {
 							return (
 								<MusicInformation key={idx}>
-									<AlbumCover src={data.imageLocation} onClick={() => LinkHandle(idx, data)}></AlbumCover>
-									{/* <Link to={"/buyer/"+idx}><AlbumCover src={data.imageLocation}></AlbumCover></Link> */}
-
-									{/* <AlbumCover src={data.imageLocation} onClick={LinkHandle}></AlbumCover> */}
-									{/* <Link to={
-                                    {
-                                        pathname:'/buyer/'+idx,
-                                        state:2
-                                    }}><AlbumCover src={data.imageLocation}></AlbumCover></Link> */}
+									{/* <AlbumCover src={data.imageLocation}></AlbumCover> */}
+									<AlbumCover src={data.imageLocation} alt="Red dot"/>
+									{/* <AlbumCover src={data.imageLocation} onClick={() => this.LinkHandle(idx, data)}></AlbumCover> */}
 									<Singer>{data.singer}</Singer>
 									<Song>{data.song}</Song>
 								</MusicInformation>
-							);
-						} else {
-							return (<TitleOfComponent>loading</TitleOfComponent>);
-						}
-					})
-					// sampleData.map((data, idx) => {
-					// 	return (
-					// 		<MusicInformation key={idx}>
-					// 			<AlbumCover src={data.imageLocation} onClick={() => LinkHandle(idx, data)}></AlbumCover>
-					// 			{/* <Link to={"/buyer/"+idx}><AlbumCover src={data.imageLocation}></AlbumCover></Link> */}
-
-					// 			{/* <AlbumCover src={data.imageLocation} onClick={LinkHandle}></AlbumCover> */}
-					// 			{/* <Link to={
-					//                 {
-					//                     pathname:'/buyer/'+idx,
-					//                     state:2
-					//                 }}><AlbumCover src={data.imageLocation}></AlbumCover></Link> */}
-					// 			<Singer>{data.singer}</Singer>
-					// 			<Song>{data.song}</Song>
-					// 		</MusicInformation>
-					// 	);
-					// })
-				}
-			</MusicInformations>
-		</Wrapper>
-	)
-=======
-					
-    
-        <Wrapper>
-            <TitleOfComponent>
-                Start Listening
-            </TitleOfComponent>
-            <MusicInformations>
-                {/* <Display ipfsPull={ipfsPull}/> */}
-                {/* {
-
-                    
-                    ipfsPull.map((data,idx)=>{
-                        return(
-                            <div>{data}</div>
-                            <MusicInformation key={idx}>
-                                     <AlbumCover src={data.imageLocation} onClick={() => LinkHandle(idx, data)}></AlbumCover>
-                                     <Singer>{data.singer}</Singer>
-                                     <Song>{data.song}</Song>
-                            </MusicInformation>   
-                        )
-                    })
-                    
-                    
-                } */}
-            </MusicInformations>
-        </Wrapper>
-    )
->>>>>>> 73c6fa495f2f756c592ee7d31dd5d5e20b3f035d
+							)
+						})
+						:
+						<TableRow>
+							<TableCell colSpan="6" align="center">
+								<CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+							</TableCell>
+						</TableRow>
+					}
+				</MusicInformations>
+			</Wrapper>
+		)
+	}
 }
-export default ShowSomeMusics;
+
+export default  withStyles(styles)(ShowSomeMusics);
+
 
 const Wrapper = styled.div`
     width: 100%;
@@ -480,17 +507,3 @@ const MusicInformation = styled.div`
     max-width:10rem;
     
 `;
-
-
-// console.log(ipfsPull["0"])
-                    // ipfsPull["0"].map((data, idx) => {
-                    //     console.log(data);
-                        // return (
-                        //     <MusicInformation key={idx}>
-                        //         <AlbumCover src={data.imageLocation} onClick={() => LinkHandle(idx, data)}></AlbumCover>
-                        //         <Singer>{data.singer}</Singer>
-                        //         <Song>{data.song}</Song>
-                        //     </MusicInformation>
-                        // );
-                    // })
-                    // ipfsPull[0].map((data,idx)=>)
